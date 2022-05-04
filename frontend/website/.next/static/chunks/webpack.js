ec165336ee7dc970791a72d18c53f4264ba157cc
asbelljc <asbelljc@gmail.com>
2022-05-04 18:07:18 -0400

    Added favicons and updated headMetadata.js accordingly.

--- frontend/website/.next/static/chunks/webpack.js
+++ frontend/website/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "06ff73b644bc3940"; }
+/******/ 		__webpack_require__.h = function() { return "a79814866175ff81"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
