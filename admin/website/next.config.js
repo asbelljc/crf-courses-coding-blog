62ff1c6b8866c26bc0702abc2cc9ec4cca408486
Jonathan Asbell <asbelljc@gmail.com>
2022-05-23 19:24:51 +0000

    Updated next config and deleted test asset animal.jpg

--- admin/website/next.config.js
+++ admin/website/next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     DEV_FRONTEND_API_URL: 'http://localhost:5000',
     DEV_ADMIN_API_URL: 'http://localhost:5001',
     PRODUCTION_ADMIN_API_URL: 'https://www.admin-api.jonathanasbell.com',
-    PRODUCTION_FRONTEND_API_URL: 'https://www.api.jonathan.com',
-    PRODUCTION_FRONTEND_WEBSITE_URL: 'https://www.jonathan.com',
+    PRODUCTION_FRONTEND_API_URL: 'https://www.api.jonathanasbell.com',
+    PRODUCTION_FRONTEND_WEBSITE_URL: 'https://www.jonathanasbell.com',
   },
 };
